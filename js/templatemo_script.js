961161b06f2f8024b89c2e972b29f143e7327e4c
McClellan Stephen R <mcclellanstephenr@johndeere.com>
2021-01-01 16:31:34 -0600

    Customize

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -3,44 +3,43 @@
 var menuDisabled = false;
 
 jQuery(function($) {
-    
+
     $(window).load(function() { // makes sure the whole site is loaded
             $('#status').fadeOut(); // will first fade out the loading animation
             $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
             $('#main-wrapper').delay(350).css({'overflow':'visible'});
     });
-    
+
     $(document).ready( function() {
 
         loadGoogleMap();
-        
+
         // backstretch for background image
         var defaultImgSrc = $('img.main-img').attr('src');
         $.backstretch(defaultImgSrc, {speed: 500});
-	
+
 	// for responsive-menu
 	$("#m-btn").click(function(){
 		$("#responsive").toggle();
 	});
-	
+
         // copy menu list to responsive menu
         var mainMenuList = $('#menu-list').html();
         $('#responsive').html(mainMenuList);
-	
+
 	//for image slide on menu item click(normal) and responsive
-	$("#menu-list a, #responsive a").on('click',function(e){
-            
-			
+	$("#menu-list a, #responsive a, a.menu-link").on('click',function(e){
+
 			if(this.className == "external") {
                 return;
             }
-			
+
 			e.preventDefault();
 
             if (menuDisabled == false) // check the menu has disabled?
             {
                 menuDisabled = true; // disable to menu
-                
+
                 var name = $(this).attr('href');
                 $('#menu-list li').removeClass('active');
                 $('#responsive li').removeClass('active');
@@ -51,11 +50,11 @@ jQuery(function($) {
 
                 // hide responsive menu
                 $("#responsive").hide();
-                
+
                 // get image url and assign to backstretch for background
                 var imgSrc = $("img"+name+"-img").attr('src');
                 $.backstretch(imgSrc, {speed: 500}); //backstretch for background fade in/out
-                
+
                 // content slide in/out
                 $("section.active").animate({left:$("section.active").outerWidth()}, 400,function(){
                     $(this).removeClass("active");
@@ -63,18 +62,18 @@ jQuery(function($) {
                     $(name+"-text").show();
                     $(name+"-text").animate({left:'0px'},400,function(){
                         $(this).addClass("active");
-                        
+
                         google.maps.event.trigger(map, 'resize'); // resize map
                         $.backstretch("resize"); // resize the background image
-                        
+
                         menuDisabled = false; // enable the menu
                     });
                 });
-                
+
             }
             return;
 	});
-        
+
     });
 
 });
